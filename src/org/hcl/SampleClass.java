0786be90d5c5e9c843e6707779581d540c3a9faf
vickyvj20 <github>
2022-05-27 10:29:52 +0530

    project

---
+++ src/org/hcl/SampleClass.java
@@ -0,0 +1,22 @@
+package org.hcl;
+
+public class SampleClass {
+	
+	private void test1() {
+	   System.out.println("test1");
+
+	}
+	
+	private void test2() {
+		System.out.println("test2");
+	}
+	
+	private void test3() {
+		System.out.println("test3");
+	}
+    
+	private void test4() {
+		System.out.println("test4");
+	}
+	
+}
